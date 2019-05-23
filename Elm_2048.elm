8d77e0f3c61e1e0d2d0837400c484d366902d257
Jacob Gold <goldjacob29@cs.uchicago.edu>
2019-05-22 19:39:30 -0500

    5-22 work

--- Elm_2048.elm
+++ Elm_2048.elm
@@ -3,83 +3,147 @@ module Elm_2048 exposing (..)
 import Matrix
 
 import Browser
+import Browser.Events
 import Html exposing (Html)
 import Random exposing (Generator)
+import Time
 import Debug
 import Html.Attributes
+import Array
+import Json.Decode as Decode
+
+-----------------------------------------------------
+-- Types and Aliases
+-----------------------------------------------------
+
+type alias Model =
+  { board : Matrix.Matrix Int }
+
+type Msg = Tick | RandomPlay Play
+
+type alias Play =
+  {index : Index, num : Num}
 
 -- top left = {0,0}
 type alias Loc = { row:Int, col:Int }
 
-type alias Model =
-  { board : Matrix.Matrix Int,
-    seed : Random.Seed }
+type alias Num = Int
+type alias Index = Int
 
 type alias Flags = ()
+-----------------------------------------------------
+-- List Functions
+-----------------------------------------------------
 
-init : Flags -> Model
-init () = initModel
+myHead : List a -> a
+myHead ls =
+  case ls of
+    (h::_) -> h
+    _ -> Debug.todo "error"
 
--- 0 = empty
-emptyBoard = Matrix.repeat 4 4 0
+myTail : List a -> List a
+myTail ls =
+  case ls of
+    (_::t) -> t
+    _ -> Debug.todo "error"
 
--- locGenerator : Generator Loc
--- locGenerator = Random.map2 Loc (Random.float 0 n)
+transpose : List (List a) -> List (List a)
+transpose ls =
+  case ls of
+    ([]::_) -> []
+    _ -> (List.map myHead ls) :: transpose (List.map myTail ls)
 
-locToNum loc = loc.row * 4 + loc.col
-numToLoc num =
-  let row = modBy 4 num
-      col = num - (row*4)
-  in Loc row col
-
-nextLoc : Loc -> Loc
-nextLoc loc = numToLoc ((locToNum loc) + 1)
-
-indexList : Int -> List a -> a
+-- find elem at index i
+indexList : Index -> List a -> a
 indexList i ls =
   case (i, ls) of
-    (_, []) -> Debug.todo "error"
-    (0, l::_) -> l
-    (_,_::rest) -> indexList (i-1) rest
+    (_, [])      -> Debug.todo "error"
+    (0, l::_)    -> l
+    (_, _::rest) -> indexList (i-1) rest
+
+-----------------------------------------------------
+-- Other Shit
+-----------------------------------------------------
+
+main : Program Flags Model Msg
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+init : Flags -> (Model, Cmd Msg)
+init () = (initModel, Cmd.none)
+
+-- 0 = empty
+emptyBoard : Matrix.Matrix Num
+emptyBoard = Matrix.repeat 4 4 0
+
+-- locToIndex : Loc -> Index
+-- locToIndex loc = loc.row * 4 + loc.col
+
+indexToLoc : Index -> Loc
+indexToLoc i =
+  let row = i // 4
+      col = i - (row*4)
+  in Loc row col
+
+-- findEmptyIndices : List Num -> Index -> List Loc
+findEmptyIndices nums index =
+  case nums of
+    [] -> []
+    (i::rest) ->
+      if i == 0 then
+        index :: findEmptyIndices rest (index+1)
+      else
+        findEmptyIndices rest (index+1)
+
+findEmpties : Matrix.Matrix Num -> List Index
+findEmpties board =
+  let boardList = Array.toList (Matrix.toArray board)
+  in findEmptyIndices boardList 0
 
 
--- findEmptySquares {0,0} mat
-findEmptySquares : Loc -> Matrix.Matrix Int -> List Loc
-findEmptySquares loc mat =
-  if locToNum loc > 15 then [] else
-  if Matrix.get loc.row loc.col mat == Result.Ok 0
-    then loc :: findEmptySquares (nextLoc loc) mat
-    else findEmptySquares (nextLoc loc) mat
+playGenerator : List Index -> Generator Play
+playGenerator empties =
+  let n = List.length empties
+  in Random.map2 Play (Random.int 0 (n-1)) (Random.weighted (75, 2) [(25,4)])
 
-pickEmptySquare : List Loc -> Random.Seed -> (Loc, Random.Seed)
-pickEmptySquare locs seed =
-  let n = List.length locs
-      (index, newSeed) = Random.step (Random.int 0 (n-1)) seed
-  in (indexList index locs, newSeed)
-
-genTwoOrFour seed = Random.step (Random.weighted (75, 2) [(25,4)]) seed
-
-placeOneVal : Loc -> Matrix.Matrix Int -> Random.Seed -> (Matrix.Matrix Int, Random.Seed)
-placeOneVal loc mat seed =
-  let (val,newSeed) = genTwoOrFour seed
-  in (Matrix.set loc.row loc.col val mat, newSeed)
+placeVal : Loc -> Num -> Matrix.Matrix Num -> Matrix.Matrix Num
+placeVal loc num board = Matrix.set loc.col loc.row num board
 
 initModel : Model
-initModel =
-  let seed = Random.initialSeed 2
-      b = emptyBoard
-      empties = findEmptySquares (Loc 0 0) b
-      (picked, seed1) = pickEmptySquare empties seed
-      (b1, seed2) = placeOneVal picked b seed1
+initModel = {board=emptyBoard}
 
-      empties1 = findEmptySquares (Loc 0 0) b1
-      (picked1, seed3) = pickEmptySquare empties1 seed2
-      (b2, seed4) = placeOneVal picked1 b1 seed3
-  in {board=b2, seed=seed4}
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick ->
+      let empties = findEmpties model.board
+      in (model, Random.generate RandomPlay (playGenerator empties))
+    RandomPlay {index, num} ->
+      let empties = findEmpties model.board
+          loc = indexToLoc (indexList index empties)
+          newBoard = placeVal loc num model.board
+          newModel = {board = newBoard}
+      in (newModel, Cmd.none)
 
 
+keyDecoder : Decode.Decoder String
+keyDecoder =
+  Decode.field "key" Decode.string
 
--- transpose : List (List a) -> List (List a)
---
--- transpose ([]::_) = []
--- transpose x = (map head x) :: transpose (map tail x)
+subscriptions : Model -> Sub Msg
+subscriptions model =
+   Browser.Events.onKeyDown (Decode.map (\key -> Tick) keyDecoder)
+
+renderList lst =
+  Html.ul []
+    (List.map (\l -> Html.li [] [ Html.text (String.fromInt l)]) lst)
+
+view : Model -> Html Msg
+view model =
+  let boardList = Array.toList (Matrix.toArray model.board)
+  in renderList boardList
