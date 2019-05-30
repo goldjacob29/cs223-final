dd7e90bcb111699cb37d964fbd244b7b22c44879
Jacob Gold <goldjacob29@cs.uchicago.edu>
2019-05-29 22:24:07 -0500

    gameplay done

--- Elm_2048.elm
+++ Elm_2048.elm
@@ -21,7 +21,7 @@ type alias Grid = List (List Num)
 type alias Model =
   { board : Grid }
 
-type Direction = Left | Right | Up | Down | Other
+type Direction = Left | Right | Up | Down | Other | NewGame
 
 type Msg = Tick | RandomPlay Play | Keystroke Direction | Gameover
 
@@ -35,6 +35,14 @@ type alias Num = Int
 type alias Index = Int
 
 type alias Flags = ()
+-----------------------------------------------------
+log : String -> a -> a
+log s x =
+  let stringX = Debug.toString x
+  in case (Debug.log s stringX) of
+    _ -> x
+
+
 -----------------------------------------------------
 -- List Helper Functions
 -----------------------------------------------------
@@ -65,24 +73,41 @@ init () = (initModel, Cmd.none)
 
 initModel : Model
 initModel = {board=emptyBoard}
+  -- let model = {board=emptyBoard}
+  --     (onePiece, _) = update Tick model
+  --     -- (twoPiece, _) = update Tick onePiece
+  -- in
+  --     onePiece
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Keystroke dir ->
-      case dir of
+      case (log "dir" dir) of
         Left  ->
-          let newBoard = left model.board
-          in update Tick {board = newBoard}
+          if canMoveLeft model.board then
+            let newBoard = left model.board
+            in update Tick {board = newBoard}
+          else (model, Cmd.none)
         Right ->
-          let newBoard = right model.board
-          in update Tick {board = newBoard}
+          if canMoveRight model.board then
+            let newBoard = right model.board
+            in update Tick {board = newBoard}
+          else (model, Cmd.none)
         Up    ->
-          let newBoard = up model.board
-          in update Tick {board = newBoard}
+          if canMoveUp model.board then
+            let newBoard = up model.board
+            in update Tick {board = newBoard}
+          else (model, Cmd.none)
         Down  ->
-          let newBoard = down model.board
-          in update Tick {board = newBoard}
+          if canMoveDown model.board then
+            let newBoard = down model.board
+            in update Tick {board = newBoard}
+          else (model, Cmd.none)
+        NewGame ->
+          let (onePiece, cmd1) =  update Tick model
+              (twoPiece, cmd2) =  update Tick onePiece
+          in (twoPiece, Cmd.batch [cmd1, cmd2])
         Other -> (model, Cmd.none)
     Tick ->
       let empties = findEmpties model.board
@@ -90,23 +115,37 @@ update msg model =
     RandomPlay {index, num} ->
       let empties = findEmpties model.board
           elem = indexList index empties
-      in
-        if elem == -1 then
-          if not (movesExist model.board) then
-            update Gameover model
-          else
-            (model, Cmd.none)
-        else
-        let
-          loc = indexToLoc (elem)
+          loc = indexToLoc (indexList index empties)
           newBoard = placeVal loc num model.board
-          newModel = {board = newBoard}
-        in (newModel, Cmd.none)
-    Gameover -> init ()
+          newModel = {board=newBoard}
+          isMove = log "move" (movesExist newBoard)
+      in
+          if not isMove then
+            update Gameover newModel
+          else
+            (newModel, Cmd.none)
+    Gameover -> update (Keystroke NewGame) initModel
+
+
+canMoveLeft : Grid -> Bool
+canMoveLeft g = left g /= g
+
+canMoveRight : Grid -> Bool
+canMoveRight g = right g /= g
+
+canMoveUp : Grid -> Bool
+canMoveUp g = up g /= g
+
+canMoveDown : Grid -> Bool
+canMoveDown g = down g /= g
 
 movesExist : Grid -> Bool
-movesExist g = False
-
+movesExist g =
+  if canMoveLeft g then True
+  else if canMoveRight g then True
+  else if canMoveUp g then True
+  else if canMoveDown g then True
+  else False
 
 keyDecoder : Decode.Decoder Direction
 keyDecoder =
@@ -119,6 +158,7 @@ toDirection string =
     "ArrowRight" -> Right
     "ArrowUp" -> Up
     "ArrowDown" ->  Down
+    " " -> NewGame --dumb shit
     _ -> Other
 
 subscriptions : Model -> Sub Msg
